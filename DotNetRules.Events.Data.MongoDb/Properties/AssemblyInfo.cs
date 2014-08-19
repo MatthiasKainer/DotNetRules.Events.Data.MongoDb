1e0a880aa6579bb343392e458cd664216041235e
MatthiasKainer <mail@matthias-kainer.de>
2014-08-19 23:04:37 +0200

    Changed assembly info

--- DotNetRules.Events.Data.MongoDb/Properties/AssemblyInfo.cs
+++ DotNetRules.Events.Data.MongoDb/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("DotNetRules.Events.Data.MongoDb")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Autoscout24")]
+[assembly: AssemblyCompany("Matthias Kainer")]
 [assembly: AssemblyProduct("DotNetRules.Events.Data.MongoDb")]
-[assembly: AssemblyCopyright("Copyright © Autoscout24 2014")]
+[assembly: AssemblyCopyright("Copyright © Matthias Kainer 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.*")]
