f91e5807ef43439cd0f29b30b1b2159fccaa1efa
Matthias Kainer <mkainer@autoscout24.com>
2014-08-19 10:06:06 +0200

    Initial commit

---
+++ DotNetRules.Events.Data.MongoDb/EventStore.cs
@@ -0,0 +1,51 @@
+namespace DotNetRules.Events.Data.MongoDb
+{
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Linq;
+
+    public class EventStore 
+    {
+        private readonly string application = ConfigurationManager.AppSettings["application.name"];
+
+        private readonly MongoSession<Event> session;
+
+        public EventStore()
+        {
+            this.session = new MongoSession<Event>();
+            Executor.RegisterObject(this);
+        }
+
+        public void AddToStore<T>(T eventItem) where T : BaseEvent
+        {
+            var @event = new Event 
+                         { 
+                             Author = eventItem.User, 
+                             ContextName = eventItem.ContextName 
+                         };
+            @event.Set(eventItem);
+            this.session.Add(@event);
+        }
+
+        public IEnumerable<BaseEvent> GetAllEvents()
+        {
+            return this.session.Queryable.Where(_ => _.ContextName == this.application).OrderByDescending(_ => _.Created).ToList().Select(_ => _.Get());
+        }
+
+        public IEnumerable<TEvents> GetEventsFromStore<TEvents>() where TEvents : BaseEvent
+        {
+            var events = this.session.Queryable.Where(_ => _.ContextName == this.application 
+                                                      && _.EventType == typeof(TEvents).AssemblyQualifiedName)
+                .OrderByDescending(_ => _.Created);
+            return events.ToList().Select(_ => _.Get<TEvents>());
+        }
+
+        public IEnumerable<TEvents> GetBoundedContextEventsFromStore<TEvents>() where TEvents : BaseEvent
+        {
+            var events = this.session.Queryable.Where(_ => _.ContextName == this.application 
+                                                      && _.EntityContextType == typeof(TEvents).AssemblyQualifiedName)
+                .OrderByDescending(_ => _.Created);
+            return events.ToList().Select(_ => _.Get<TEvents>());
+        }
+    }
+}
---
+++ DotNetRules.Events.Data.MongoDb/MongoSession.cs
@@ -0,0 +1,64 @@
+﻿namespace DotNetRules.Events.Data.MongoDb
+{
+    using System.Configuration;
+    using System.Linq;
+
+    using MongoDB.Driver;
+    using MongoDB.Driver.Linq;
+
+    class MongoSession<T>
+    {
+        private readonly MongoDatabase database;
+
+        public MongoSession()
+            : this(ConfigurationManager.ConnectionStrings["DotNetRules.Events.Data.MongoDb"].ConnectionString)
+        {
+        }
+
+        public MongoSession(string connectionString)
+        {
+            var url = new MongoUrl(connectionString);
+            var client = new MongoClient(url);
+            this.database = client.GetServer().GetDatabase(url.DatabaseName);
+            this.EnsureCollection();
+        }
+
+        private void EnsureCollection()
+        {
+            if (!this.database.CollectionExists(typeof(T).Name)) this.database.CreateCollection(typeof(T).Name);
+        }
+
+        public IQueryable<T> Queryable
+        {
+            get
+            {
+                return this.database.GetCollection<T>(typeof(T).Name).AsQueryable<T>();
+            }
+        }
+
+        public void Add(T item)
+        {
+            this.database.GetCollection<T>(typeof(T).Name).Insert(item, SafeMode.True);
+        }
+
+        public void Update(T item)
+        {
+            this.database.GetCollection(typeof(T).Name).Save(item, SafeMode.True);
+        }
+
+        public void Update(IMongoQuery query, IMongoUpdate documentToUpdate)
+        {
+            this.database.GetCollection<T>(typeof(T).Name).Update(query, documentToUpdate);
+        }
+
+        public void Delete<TType>(IMongoQuery query)
+        {
+            this.database.GetCollection<TType>(typeof(TType).Name).Remove(query);
+        }
+
+        public void Save(T item)
+        {
+            this.database.GetCollection<T>(typeof(T).Name).Save(item, SafeMode.True);
+        }
+    }
+}
